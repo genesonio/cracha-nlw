50448df869a7c34ecdaec8f569ac51245cdd1112
Genésio <genesio.s.p.28@gmail.com>
2022-04-25 17:43:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const LinksSocialMedia = {
+  github: 'genesonio',
+  youtube: 'c/rocketseat',
+  instagram: 'genesonio',
+  twitter: 'maykbrito',
+  facebook: 'genesonio'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+function changeUserName() {
+  const Name = userName.innerText
+  userName.innerText = ``
+}
+// changeUserName()
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      ;(userName.textContent = `${data.name}`),
+        (userBio.textContent = `${data.bio}`),
+        (userLink.href = `${data.html_url}`),
+        (profilePic.src = `${data.avatar_url}`),
+        (userLogin.textContent = `${data.login}`)
+    })
+}
+
+getGitHubProfileInfos()
